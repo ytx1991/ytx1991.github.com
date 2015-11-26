4f04fb32ea12e430c0de500b6494472b2214207f
Qiming Chen <qimingchen@Qimings-MacBook-Pro.local>
2015-11-25 23:50:46 -0800

    add GFS

--- dict.js
+++ dict.js
@@ -123,6 +123,7 @@ var USC_building = [
 		["NML", "NFL", "MMA", "an ml", "Norris Medical Library"],
 		["KAP", "Kaprielian Hall"],
 		["SGM", "Seeley G. Mudd Building"],
+		["GFS", "Grace Ford Salvatori Hall"],
 		["ANH", "ANS", "Annenberg House", "711 West 27th Street Los Angeles, CA 90007"],
 		["BAA", "be a egg", "bae", "Bel-Air Apartments", "1124 West 29th Street Los Angeles, CA 90007"],
 		["CAR", "Cardinal Gardens", "3131 McClintock Avenue Los Angeles, CA 90007"],
